c24298b7f494a152ba2c23ae777e39429a345ab7
Emmanuel Martin <em.martin456@gmail.com>
2025-10-23 13:58:12 +0200

    utilisation de useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,65 @@
-
+import { useState, useEffect,useMemo } from 'react'
+import axios from 'axios'
+import Profile from './components/Profile'
 import './App.css'
 
 function App() {
 
 
+  const [count, setCount] = useState(1);
+  const [profile, setProfile] = useState({})
+  const [dark, setDark] = useState(false)
+
+  useEffect(() => {
+    axios
+    .get(`https://jsonplaceholder.typicode.com/users/${count}`)
+    .then((response) => {
+        setProfile(response.data)
+
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  }, [count])
+
+
+  const memoizeValue = useMemo(() => {
+    console.log("Je suis dans isOverTen")
+    return count > 10
+  }, [count])
+
+  console.log(memoizeValue)
+
+
+  const goDark = () => {
+    setDark(!dark)
+
+    if (!dark) {
+      document.body.classList.add("bg-secondary")
+    } else {
+      document.body.classList.remove("bg-secondary")
+    }
+  }
+
+  const classButtonTheme = dark ? "btn-light" : "btn-dark"
+  const textButtonTheme = dark ? "Mode clair" : "Mode sombre"
+
   return (
-    <div>
-     
-    </div>
+   <div className="container">
+    <h1 className="text-center">useMemo()</h1>
+
+    {
+      memoizeValue && <div className="alert alert-danger" role="alert">STOP!!!</div>
+    }
+
+    <button className="btn btn-info m-3 text-white" onClick={() => setCount(count + 1)}>Increment {count}</button>
+
+    <button className={`btn ${classButtonTheme} m-3 float-right`} onClick={goDark}>{textButtonTheme}</button>
+
+    <Profile count={count} profile={profile}/>
+
+   </div>
   )
 }
 
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import { memo } from "react";
+
+
+const Profile = ({count, profile}) => {
+
+  console.log("je suis dans profile")
+  return (
+    <div>
+      <ul className="list-group">
+        <li className="list-group-item"><p className="h2">User ID:{count} </p> </li>
+        <li className="list-group-item"> <strong>Nom:  </strong>{profile.name} </li>
+        <li className="list-group-item"><strong>Pseudo: </strong> {profile.username}</li>
+        <li className="list-group-item"><strong>Email: </strong> {profile.email} </li>
+      </ul>
+    </div>
+  )
+}
+
+export default memo(Profile);
